3ad37c3a681e385d8195e64d8fc500074b0d276c
hshan608 <co_co_98@naver.com>
2022-07-02 17:42:03 +0900

    앱 레이아웃 및 버튼 Intent 추가

--- app/src/main/java/com/example/healingment/MainActivity.java
+++ app/src/main/java/com/example/healingment/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.healingment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button name_friend_btn = (Button) findViewById(R.id.friendbtn);
+        name_friend_btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), FriendActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
